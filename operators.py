e755fba484f9720b1b8def5d2a80559d5d0b4920
Avi Budha <basanta231buddha@gmail.com>
2024-07-02 13:08:14 +0545

    ppython is the best

---
+++ operators.py
@@ -0,0 +1,177 @@
+# # arthmetic operators is the boss here 
+# #i can do anything with these operators 
+# #variables 
+# x = 20
+# y = 10
+
+# #addition 
+# print(x + y)
+
+# #substraction 
+# print(x - y)
+
+# #multiplication 
+# print(x*y)
+
+# #division 
+# print(x/y)
+
+# # modulus
+# print(x%y)
+
+# #exponentation
+# print(x**y)
+
+# #floor division 
+# print(x//y)
+
+
+#assignment operators 
+#assign 
+#add and assign
+# x += 6
+# print("x += 5 ->", x)
+# subtract and assign
+# multiply and assign 
+# divide and assign
+# modulus and assign 
+# expo and assign
+
+
+
+# # comparisn operators 
+# num1 = 23
+# num2 = 26
+
+# print(num1 != num2)
+# print(num1 == num2)
+# print(num1 < num2)
+# print(num1 > num2)
+# print(num1 <= num2)
+# print(num1 >= num2)
+
+
+#logical operators 
+
+# a = True 
+# b = False
+# print(a and b)
+# print(a or b) # not good 
+# print(not b)
+
+# identity operators are used to compare the object, not if they are equal , but if they are actually the samea object, with the same memory allocation 
+
+# a = [1, 2, 3]
+# b = a
+# c = [1, 2, 3]
+# print( a is b ) # true, because both a and b point tpo the same object 
+# print(a is c)
+# print(b is c)
+
+
+
+ #sequence  operators 
+# fruit = "Mango"
+# print("a" in fruit)
+# print("g" in fruit )
+# alt + shift direct under 
+
+
+#operator presidence 
+# equ = 2 + 3 * 6 / 4 
+# print( equ)
+
+# ternary operators in python 
+# syntax : [on_true] if [expression] else [on_false]
+
+# a, b= 30, 20 # ( a=10,b= 20 )
+# min = a if a < b else b 
+# print (min) 
+
+# i = 15
+# if(i>14):
+#     print('i love someone ')
+# else:
+#     print('i dont love her')
+
+ 
+#  name = "john" 
+# if "john" == name:
+#  print('my name is john')
+# else:
+#  print('not so ')
+
+
+# x = 40
+# name = "devid"
+# if ('v' in name ):
+#     print("above ten devid ")
+# if x > 20:
+#     print("above twenty")
+# else:
+#     print("but not above 20")
+
+
+# i = 10 #(10 is the intiger )
+# if (i == 10):  
+#     print("i is 10")
+# elif (i == 15):
+#     print(" i is 15")
+# elif (i == 25):
+#     print("i is 25")
+# elif (i == 35):
+#     print("i is teh boss")
+# else:
+#     print(" it is not found")
+
+
+# if True:
+#     pass 
+# elif i==20:
+#     print("it is true")
+
+
+# name = input("what is your name:")
+# print("welcome ", name)
+
+# 8 num ko question 
+# amount=0
+# num=int(input("enter number of electric bill:"))
+# if num<=100:
+#     amount=0
+# if num>100 and num<=200:
+#     amount=(num-100)*5
+# if num>200:
+#     amount=500+(num-200)*10
+# print("amount to pay:", amount)
+
+
+#tax payment 
+# tax = 0
+# pr=int(input("enter the price of bike :"))
+# if pr > 100000:
+#     tax = 15/100*pr
+# elif pr > 500000 and pr <= 100000:
+#     tax = 10/100*pr
+# else:
+#     tax = 5/100*pr
+# print("tax to be paid", tax)
+
+
+
+# text = ("avi boss")
+# for i in text:
+#     print(i)
+
+# fruit = ["apple", "banana", "mango"]
+# for index in range(len(fruit)):
+#     print(fruit[index])
+# else:
+#     print(len(fruit)
+
+
+
+
+
+
+print("bye")
---
+++ start.py
@@ -0,0 +1,36 @@
+
+# # write a program to create variables with litrels of 1. int,float,string,boolean,
+# # {print the type of each variable print the value of each variable 
+# # print the value of each variable in the sentence using camel case or pascal case}
+
+
+# name =  "suraj"
+# age = 19
+# pendingfees = True
+# GPA = 3.8
+# print(name,age,pendingfees,GPA)
+
+# #type of each variables 
+# print("type of name", type(name))
+# print("type of age", type(age))
+# print("type of pendingfees", type(pendingfees))
+# print("type of GPA", type(GPA))
+
+# #print the value of each variable 
+# print("value of name: {name}")
+# print("value of age: {age}")
+# print("value of pendingfees: {pendingfees}")
+# print("value of GPA: {GPA}")
+
+# #print the value of each variable ina  sentence 
+# print(f"the string variable says: '{name}'")
+
+
+# #python operators
+
+print("avi")
+
+
+
+
+
